3622208652e53b7057673d20f3ba9daf0f9474b6
Christopher Espy <github@konchris.de>
2016-10-26 19:29:29 +0200

    Added first small cli scripts

---
+++ supreme-broccoli/cli_test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+""" The main script.
+
+"""
+
+__author__ = "Christopher Espy"
+__copyright__ = "Copyright (C) 2016, Christopher Espy"
+__credits__ = ["Christopher Espy"]
+__license__ = "GPLv3"
+__version__ = "0.1"
+__maintainer__ = "Christopher Espy"
+__email__ = "github@konchris.de"
+__status__ = "Development"
+
+from hw import hw
+
+def main():
+    print(hw())
+
+if __name__ == "__main__":
+    main()
